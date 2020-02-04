1ddbd0b13b8cc69dc5f67cd09ecf161fabd846d9
MEDNEXT\apaneras <apaneras@mrhealthtech.com>
2020-02-04 10:55:10 +0200

    readme & configs

--- src/Config.js
+++ src/Config.js
@@ -7,15 +7,15 @@ export const showView = {
 
 //ENABLE REST API TO YOUR WOOCOMMERCE STORE AND PROVIDE THE INFOS
 const rest_api_creds = {
-  website : "https://lovefashionpoint.gr",  // WEBSITE URL
-  consumer_key: "ck_74731bc686c7a4c3a5a26110d7d813b8268d5b83",  // CONSUMER KEY
-  consumer_secret: "cs_562201b3c09564fce2ad01c8b52c96d6e54b5851",  // CONSUMER SECRET
+  website : "",  // WEBSITE URL
+  consumer_key: "",  // CONSUMER KEY
+  consumer_secret: "",  // CONSUMER SECRET
 }
 
 //ANALYTICS CREDS AND CONFIGURATION
 export const analytics = {
-  client_id : "462148689287-omlkrm6phhnahdkr4vdqam352t3sujpn.apps.googleusercontent.com",
-  view_id: "133587325",
+  client_id : "",
+  view_id: "",
   groupSources : true //show all sources from facebok & instagram as one parent source ( e.x => i.facebook , m.facebook etc)
 }
 
